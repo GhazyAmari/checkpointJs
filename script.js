7c0f14e14ab06407f73a9919d059ab474924c42d
GhazyAmari <ghazyamari06@gmail.com>
2024-11-26 12:04:38 +0100

    updated

--- script.js
+++ script.js
@@ -151,18 +151,25 @@
 // console.log(`${number} is prime: ${isPrime(number)}`);
 
 // ex 9
-function generateFibonacci(n) {
-    if (n <= 0) return [];
-    if (n === 1) return [0];
+// function generateFibonacci(n) {
+//     if (n <= 0) return [];
+//     if (n === 1) return [0];
   
-    const fibonacci = [0, 1];
-    for (let i = 2; i < n; i++) {
-      fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
-    }
-    return fibonacci;
-  }
+//     const fibonacci = [0, 1];
+//     for (let i = 2; i < n; i++) {
+//       fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+//     }
+//     return fibonacci;
+//   }
   
-  console.log(generateFibonacci(6)); 
+//   console.log(generateFibonacci(6)); 
   
+const text = "hello world! we are ready to go"
 
+function cap(str){
+  const t = (w) => w[0].toUpperCase() + w.slice(1);
+  return result = text.split(' ').map(t).join(' ');
+  
+}
 
+console.log(cap(text));
